345b8801604fec127342982f3d4161893584f1ae
NihlaNazim <nihla.f@bluebrand.org>
2025-09-18 12:24:34 +0530

    fix the bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import About from "./components/About/about";
 import ProjectGrid from "./components/projects/ProjectGrid";
 import Timeline from "./components/experience/Timeline";
 import Education from "./components/education/Education";
-import Certifications from "./components/Certifications/certifications";
+import Certifications from "./components/certification/Certifications";
 import Skills from "./components/skills/Skills";
 import ContactForm from "./components/contact/ContactForm";
 import Footer from "./components/layout/Footer";
