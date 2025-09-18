bd64e77ed5948e36259b8e2803b0ed8103872d93
NihlaNazim <nihla.f@bluebrand.org>
2025-09-18 12:15:18 +0530

    added the workflow of github to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/New_Portfolio/',   // ← repo name (keep the trailing slashes as shown)
 })
