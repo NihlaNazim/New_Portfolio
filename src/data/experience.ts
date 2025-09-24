7ba82f272d496dd5a0e7271642e23bad099a3e72
NihlaNazim <nihla.f@bluebrand.org>
2025-09-24 07:51:16 +0530

    fix the typescript error

--- src/data/experience.ts
+++ src/data/experience.ts
@@ -4,7 +4,7 @@ export type Experience = {
     start: string; // "2023-05"
     end: string;   // "Present" or "2024-11"
     bullets: string[];
-    link?: string | string[];
+    link?: string; // <- make it just a string
 };
 
 export const EXPERIENCE: Experience[] = [
@@ -17,7 +17,7 @@ export const EXPERIENCE: Experience[] = [
         "Built modern BMS data pipeline (Python, MQTT, TimescaleDB) with Docker.",
         "Implemented CI/CD and container orchestration for Yocto-based devices.",
         ],
-        link: "https://bluebrand.org"
+        link: "https://bluebrand.org",
     },
     {
         company: "Freelance",
@@ -28,8 +28,8 @@ export const EXPERIENCE: Experience[] = [
         "Developed and maintained websites for local businesses using HTML, CSS, JavaScript, and WordPress.",
         "Created engaging video content for social media platforms using Capcut, Canva.",
         "Did some wireframing and designing for websites using Figma.",
-        "Collaborated with clients to understand their requirements and deliver customized solutions.(like logo designing, banner designing, video editing, etc.)"
+        "Collaborated with clients to understand their requirements and deliver customized solutions.(like logo designing, banner designing, video editing, etc.)",
         ],
-        link: "https://www.linkedin.com/in/nihla-nazim?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMF%2FVWXyHSXOS9j2Zth%2FhQw%3D%3D"
-    }
+        link: "https://www.linkedin.com/in/nihla-nazim?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BMF%2FVWXyHSXOS9j2Zth%2FhQw%3D%3D",
+    },
 ];
